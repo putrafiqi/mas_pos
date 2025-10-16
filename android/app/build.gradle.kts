64df9c847a0964bf2bf19ab3206c158f286836bb
Putra Afiqi <pikiyolputra@gmail.com>
2025-10-17 04:17:34 +0700

    update app icons and permissions, add launcher icon configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.mas.pos.mas_pos"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
